e78f3041a38f9712b47ede121a210a9060f71c0c
azinre <azin.rg@gmail.com>
2023-07-10 11:03:07 -0700

    client update

--- client/src/components/InputBoard.js
+++
@@ -1,9 +0,0 @@
-import React, {Fragment} from "react";
-const InputBoard= () => {
-    return
-        <Fragment>
-            <h1> select yor cartboard</h1>        
-        </Fragment>
-}
-
-export default InputBoard; 
